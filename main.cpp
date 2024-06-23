712dcd33392bd737596d6370dd56e16c9909c74a
Koper-student <237988@edu.p.lodz.pl>
2024-06-23 02:20:54 +0200

    Add stack operations to menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include "Queue.h"
+#include "Stack.h"
 
 void menu(){
     printf("\n");
@@ -8,7 +9,12 @@ void menu(){
     printf("3 - delete first value from queue\n");
     printf("4 - check if queue if empty\n");
     printf("5 - check if queue is full\n");
-    printf("6 - EXIT\n");
+    printf("6 - put value on stack\n");
+    printf("7 - delete value from stack\n");
+    printf("8 - get top value from stack\n");
+    printf("9 - check if stack is empty\n");
+    printf("10 - check size of stack\n");
+    printf("11 - EXIT\n");
     printf("\n");
 }
 
@@ -23,64 +29,100 @@ int main(){
 
 
         switch (option){
-            case 1:
-                if ( !isQueueFull() ){
+            case 1: {
+                if (!isQueueFull()) {
                     printf("Give value: ");
                     scanf("%d", &temp);
                     Insert(temp);
-                }
-                else {
+                } else {
                     printf("Queue is full!!!\n\n");
                 }
 
                 break;
+            }
 
-            case 2:
+            case 2: {
                 if (!isQueueEmpty()) {
                     temp = First();
                     printf("First value: %d", temp);
-                }
-                else {
+                } else {
                     printf("Queue is empty!!!\n\n");
                 }
 
                 break;
+            }
 
-            case 3:
+            case 3: {
                 if (!isQueueEmpty()) {
                     temp = Delete();
                     printf("Deleted value: %d", temp);
-                }
-                else {
+                } else {
                     printf("Queue is empty!!!\n\n");
                 }
                 break;
+            }
 
-            case 4:
+            case 4: {
                 if (isQueueEmpty()) {
                     printf("Queue is empty.\n");
-                }
-                else {
+                } else {
                     printf("Queue is NOT empty.\n");
                 }
                 break;
+            }
 
-            case 5:
+            case 5: {
                 if (isQueueFull()) {
                     printf("Queue is full.\n");
-                }
-                else {
+                } else {
                     printf("Queue is NOT full.\n");
                 }
                 break;
+            }
 
-            case 6:
+            case 6: {
+                printf("Give value: ");
+                scanf("%d", &temp);
+                push(temp);
+                break;
+            }
+
+            case 7: {
+                int stack_value = pop();
+                printf("Popped value from stack: %d\n", stack_value);
+                break;
+            }
+
+            case 8: {
+                int top_value = top();
+                printf("Top value from stack: %d\n", top_value);
+                break;
+            }
+
+            case 9: {
+                if (isEmpty()) {
+                    printf("Stack is empty");
+                } else {
+                    printf("Stack is NOT empty");
+                }
+                break;
+            }
+
+            case 10: {
+                int stack_size = size();
+                printf("Stack size: %d", stack_size);
+                break;
+            }
+
+            case 11: {
                 exited = 1;
                 break;
+            }
 
-            default:
+            default: {
                 printf("Incorrect input.\n\n");
                 break;
+            }
         }
     }
 
